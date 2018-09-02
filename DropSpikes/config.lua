7067949e2c5e6bc0243a49aed8e5f7597326dc30
BluemanGaming <39203615+BluemanGaming@users.noreply.github.com>
2018-09-02 20:30:24 +0200

    Update config.lua

--- DropSpikes/config.lua
+++ DropSpikes/config.lua
@@ -9,6 +9,14 @@ SpikeConfig = {}
 SpikeConfig.PedRestriction = false
 SpikeConfig.PedList = {
     "s_m_y_cop_01"
+	"s_f_y_cop_01"
+	"s_m_y_hwaycop_01"
+	"s_m_y_sheriff_01"
+	"s_f_y_sheriff_01"
+	"s_m_y_ranger_01"
+	"s_f_y_ranger_01"
+	"s_m_y_fireman_01"
+	"s_m_m_paramedic_01"
 }
 
 
